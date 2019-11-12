0e7a350c2740fd7c1c7ffdda59e8a68cbdf8c47d
Dawid Czarczynski <dawidczarczynski@gmail.com>
2019-11-12 11:01:17 +0100

    Add webpack configuration for backend functions

---
+++ backend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  target: 'node',
+  entry: {
+    changeHeaterStatus: path.resolve(__dirname, './changeHeaterStatus/index.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin()
+    ],
+    alias: {
+      shared: path.resolve(__dirname, '../shared'),
+      contract: path.resolve(__dirname, '../contract')
+    }
+  },
+  optimization: {
+    minimize: false
+  },
+  devtool: 'source-map',
+  output: {
+    filename: '[name]/index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs'
+  }
+};
