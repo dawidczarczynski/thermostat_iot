58425ae3246444f8450ef99814ae5331e287b6ec
Dawid Czarczynski <dawidczarczynski@gmail.com>
2019-11-27 21:51:28 +0100

    Add basic heater controller screen

--- client_android/app/src/main/java/com/dawidczarczynski/heater/MainActivity.kt
+++ client_android/app/src/main/java/com/dawidczarczynski/heater/MainActivity.kt
@@ -2,11 +2,24 @@ package com.dawidczarczynski.heater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.sdsmdg.harjot.crollerTest.Croller
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var heaterController: Croller
+    private lateinit var temperatureLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        heaterController = findViewById(R.id.heaterController)
+        temperatureLabel = findViewById(R.id.temperatureLabel)
+
+        heaterController.setOnProgressChangedListener{
+            val celsiusDegrees = "$it°"
+            temperatureLabel.text = celsiusDegrees
+        }
     }
 }
